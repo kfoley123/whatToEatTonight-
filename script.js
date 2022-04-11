630b5a10d8e56125d3651c56585be5d285363405
Kortney <kortney@iMac.local>
2022-04-11 20:50:00 -0300

    created recipe container which stores the recipe info we get back. Wrote a fetch that grabs a random meal from the api and prints its name to the recipe container inner HTML.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const getFoodButton = document.querySelector(".getFood");
+const recipeContainer = document.querySelector(".recipeContainer");
 
 getFoodButton.addEventListener("click", buttonClick);
 
 function buttonClick() {
-    console.log("it works!");
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then((response) => response.json())
+        .then((response) => {
+            meal(response.meals[0]);
+        });
 }
+
+const meal = (recipe) => {
+    const recipeInnerHTML = `${recipe.strMeal}`;
+    recipeContainer.innerHTML = recipeInnerHTML;
+};
