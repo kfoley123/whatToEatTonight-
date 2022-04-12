cb872b42792761102bf2daa2552365f833716709
Kortney <kortney@iMac.local>
2022-04-11 21:15:06 -0300

    created variables that grab parts of the JSON object i.e name, catergory etc. and create a new innerHTML that uses those variables to display to the screen.

--- script.js
+++ script.js
@@ -12,6 +12,22 @@ function buttonClick() {
 }
 
 const meal = (recipe) => {
-    const recipeInnerHTML = `${recipe.strMeal}`;
+    var str = JSON.stringify(recipe);
+    var recipeName = recipe.strMeal;
+    var recipeCategory = recipe.strCategory;
+    var instructions = recipe.strInstructions;
+    var foodType = recipe.strArea;
+    var ingredients = [];
+
+    for (var i = 1; i < 20; i++) {
+        ingredients.push(recipe.strIngredient1);
+    }
+
+    const recipeInnerHTML = `
+        <h1>${recipeName}</h1>
+        <h3> ${recipeCategory} </h3>
+        <p> ${ingredients} </p>
+        <p> ${instructions} </p>
+        `;
     recipeContainer.innerHTML = recipeInnerHTML;
 };
